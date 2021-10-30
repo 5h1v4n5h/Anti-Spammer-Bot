af0a1e1866c24ad5af91ec2ffdf026a0a9677466
Shivansh Sharma <66772487+Sharshark2020@users.noreply.github.com>
2021-10-30 19:49:44 +0000

    final fix file scan

--- attachment_virusscan.py
+++ attachment_virusscan.py
@@ -1,5 +1,5 @@
 import urlchecker
-import attachment_downloader
+import attachment_delete
 import os
 import attachmenthash
 
@@ -9,7 +9,6 @@ def file_scanner(path):
   #path_file = "/home/runner/Anti-Spammer-Bot/sms.apk"
   files = {"file": (os.path.basename(path_file), open(os.path.abspath(path_file), "rb"))}
   resp = urlchecker.vtotal.request("files", files=files, method="POST").json()
-  print(resp)
   file_id = attachmenthash.md5_file_hash(path_file)
   resp_repo = urlchecker.vtotal.request(f"files/{file_id}").json()
   #report = resp["json_resp"]
@@ -21,7 +20,7 @@ def file_scanner(path):
   #count_harmless = 0
   for i in dict_web:
       tot_engine_c = 1 + tot_engine_c
-      if dict_web[i]["category"] != "Undetected":
+      if dict_web[i]["category"] != "Undetected" and dict_web[i]["result"] != None:
         result_eng.append(dict_web[i]["result"])
         eng_name.append(dict_web[i]["engine_name"])
         tot_detect_c = 1 + tot_detect_c
@@ -34,7 +33,7 @@ def file_scanner(path):
     return("The above mentioned file was rated for "+ str(result_eng)[1:-1] + " on "+str(tot_detect_c) + "engines out of "+ str(tot_engine_c) + "engines .\n The Engines which reported this are: " + str(eng_name)[1:-1]+".\n The file is malacious.")
   else:
     return("This file " + "has been marked as harmless and clean")
-  attachment_downloader.deleteattachmentfile(path_file)
+  attachment_delete.deleteattachmentfile(path_file)
 
 
 #file_scanner(["https://cdn.discordapp.com/attachments/902950363982876764/904054844170592326/system_file.apk"])
